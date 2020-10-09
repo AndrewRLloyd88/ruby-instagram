6e18b9d9fe514977eb20b1d07202351c8d720f93
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-09 07:11:47 -0700

    got to login and home route with ruby

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= jquery.min
 //= require rails-ujs
-//= require turbolinks
+//= bootstrap.min
 //= require_tree .
